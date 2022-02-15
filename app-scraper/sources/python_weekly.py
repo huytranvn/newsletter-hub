74934938c5468a3cd94532535b0b401dc4f69521
Huy Tran <huytran.code@gmail.com>
2022-02-15 16:55:19 +0700

    Finish implement the first source.

--- app-scraper/sources/python_weekly.py
+++ app-scraper/sources/python_weekly.py
@@ -1,9 +1,6 @@
 import os
 import re
 
-from typing import Optional
-from urllib.request import urlopen
-
 from bs4 import BeautifulSoup
 
 from models.schemas import (
@@ -22,6 +19,8 @@ class PythonWeekly:
     ANCHOR_TEXT_TITLE = '^Welcome\sto\sissue\s[0-9]{1,3}\sof\sPython\sWeekly'
     ANCHOR_TEXT_ARTICLE = 'Articles,\sTutorials\sand\sTalks'
     ANCHOR_TEXT_LIBRARY = 'Interesting\sProjects,\sTools\sand\sLibraries'
+    ANCHOR_TEXT_NEW_RELEASES = 'New\sReleases'
+    ANCHOR_TEXT_UPCOMING_EVENTS = 'Upcoming\sEvents\sand\sWebinars'
 
     PAGE_URL = 'https://www.pythonweekly.com/archive/{0}.html'
     PAGE_NAME = 'python_weekly_{0}.html'
@@ -39,8 +38,19 @@ class PythonWeekly:
             number=issue_number,
             columns=[],
         )
+
+        # Create categories.
         self.category_article = Category(
             name='article',
+        )
+        self.category_library = Category(
+            name='library',
+        )
+        self.category_new_release = Category(
+            name='new_release',
+        )
+        self.category_upcoming = Category(
+            name='upcoming',
         )
 
         # Scrape the page.
@@ -51,6 +61,7 @@ class PythonWeekly:
 
         # Init flags.
         self.column = None
+        self.previous_item = None
 
         # Scrape the webpage.
         scraper.scrape()
@@ -68,6 +79,18 @@ class PythonWeekly:
         pass
 
     def save_article(self, item: BeautifulSoup) -> None:
+        return self.save_item(item=item, category=self.category_article)
+
+    def save_library(self, item: BeautifulSoup) -> None:
+        return self.save_item(item=item, category=self.category_library)
+
+    def save_new_release(self, item: BeautifulSoup) -> None:
+        return self.save_item(item=item, category=self.category_new_release)
+
+    def save_upcoming_events(self, item: BeautifulSoup) -> None:
+        return self.save_item(item=item, category=self.category_upcoming)
+
+    def save_item(self, item: BeautifulSoup, category: Category) -> None:
         # Try to find a tag.
         hyperlink_tag = item.find('a')
 
@@ -80,11 +103,14 @@ class PythonWeekly:
             self.column = Column(
                 link=link,
                 title=link.text.strip(),
-                category=self.category_article,
+                category=category,
                 description=None,
                 tags=[],
             )
-        elif self.column:
+            self.previous_item = item
+        elif self.column \
+                and item.name == 'div' \
+                and self.previous_item.text.strip() != item.text.strip():
             # This must be the description.
             self.column.description = item.text.strip()
             self.issue.columns.append(self.column)
@@ -101,17 +127,27 @@ class PythonWeekly:
                     .find('table', id='contentTable') \
                     .find('table', class_='bodyTable') \
                     .find('td', class_='defaultText') \
-                    .find_all('div')
+                    .find_all(re.compile(r'div|p'))
 
         article_start = False
         article_end = False
         library_start = False
         library_end = False
+        new_release_start = False
+        new_release_end = False
+        upcoming_start = False
+        upcoming_end = False
 
         for item in items:
             if article_start and not article_end:
                 # Getting articles.
                 self.save_article(item)
+            elif library_start and not library_end:
+                self.save_library(item)
+            elif new_release_start and not new_release_end:
+                self.save_new_release(item)
+            elif upcoming_start and not upcoming_end:
+                self.save_upcoming_events(item)
 
             text = item.text.strip()
             if re.search(self.ANCHOR_TEXT_TITLE, text):
@@ -120,3 +156,10 @@ class PythonWeekly:
                 article_start = True
             elif re.search(self.ANCHOR_TEXT_LIBRARY, text):
                 article_end = True
+                library_start = True
+            elif re.search(self.ANCHOR_TEXT_NEW_RELEASES, text):
+                library_end = True
+                new_release_start = True
+            elif re.search(self.ANCHOR_TEXT_UPCOMING_EVENTS, text):
+                new_release_end = True
+                upcoming_start = True
